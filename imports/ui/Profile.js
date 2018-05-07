6459bdd1b8f7a4e8fabacd522fd5665036706650
Mark Thrasher <drmarkthrasher@gmail.com>
2018-05-07 18:28:38 -0500

    Simple change

--- imports/ui/Profile.js
+++ imports/ui/Profile.js
@@ -24,13 +24,37 @@ class Profile extends Component {
         return (
             
             <div className="page-content">
-                <label className="primaryfont">Name</label>
-                <input className='autoExpand form-input' rows='1' data-min-rows='1'
-                    id="name" 
-                    placeholder=''
-                    onChange={this.handleChange.bind(this)}
-                    value={this.state.name}>
-                </input>
+
+                <h2 className="form-section_title">Personal Information</h2>
+                <div className="form-section">
+                    
+                        <label>Name</label>
+                        <input className='autoExpand form-input' rows='2' data-min-rows='2'
+                            id="name" 
+                            placeholder=''
+                            onChange={this.handleChange.bind(this)}
+                            value={this.state.name}>
+                        </input>
+                        <br/><br/>
+                        Sex:   Male <input type="radio" name="gender"/>  Female <input type="radio" name="gender" /><br/>
+                        <br/>
+                        <label>Age</label>
+                        <input className='autoExpand form-input' rows='2' data-min-rows='2'
+                            id="age" 
+                            placeholder=''
+                            onChange={this.handleChange.bind(this)}
+                            value={this.state.age}>
+                        </input>
+
+                        <label>Weight</label>
+                        <input className='autoExpand form-input' rows='2' data-min-rows='2'
+                            id="weight" 
+                            placeholder=''
+                            onChange={this.handleChange.bind(this)}
+                            value={this.state.weight}>
+                        </input>
+                </div>
+
         
             </div>
         );
